e297cc51fc6be79b490eb54008ddca3542162c92
Neal Pisenti <npisenti@gmail.com>
2011-07-30 19:18:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usptosis::Application.config.secret_token = '434f7bc3b253ecc394f88508a44e5703c198e3b4e3d69257388cd9d143ad76fd91ded7222e7a1d5227de7893d4bb8512b606e84376738d3b49493a2b725cbefa'
