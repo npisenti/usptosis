657fad9f289bce134419385ac90b6485bb08b428
Neal Pisenti <npisenti@gmail.com>
2011-07-30 20:12:58 -0700

    mapped root route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Usptosis::Application.routes.draw do
 
   match '/home', :to => 'pages#home'
-
+  root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
