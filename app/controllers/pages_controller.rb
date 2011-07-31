3f1440b183cc7bdad37cdfd15e2c9b61dde2e9e7
Neal Pisenti <npisenti@gmail.com>
2011-07-31 11:08:24 -0700

    added randomization to ordering of patent names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   def home
     @fake = FakePatent.all.sample
     @real = RealPatent.all.sample
+    @patent_array = [@fake, @real].shuffle
   end
 end
