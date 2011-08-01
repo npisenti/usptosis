4e52bd7749b5eac7c39b6c403075a9e827e46317
Neal Pisenti <npisenti@gmail.com>
2011-08-01 09:00:20 -0700

    added some javascript chekc answer buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+$(document).ready(function(){
+  $("#check-button").button();
+  $("#roll-again").button();
+  $("#roll-again").hide();
+
+  $("#check-button").click(function(){ 
+    if ($("#answer").text() == "0"){
+      $(".left").css('background', 'green');
+      } else {
+      $(".right").css('background', 'green');
+      }
+     $("#roll-again").show(); 
+     $("#check-button").hide();
+    });
+$("#roll-again").click(function(){
+  window.location.reload();
+  });
+});
